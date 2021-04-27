b3a82bc1feb9c1b229def44f430a8eb4a2e58e97
Alvaro Barrera <alvaro_bv@hotmail.com>
2021-04-26 20:39:18 -0500

    Updates

---
+++ precache-manifest.bb1db8d2ad7ce9aeb2d588cf8bf0a9ab.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "8de8b80c514d1b560ea2",
+    "url": "/alvaro-barrera/static/css/main.cb9ecca7.chunk.css"
+  },
+  {
+    "revision": "8de8b80c514d1b560ea2",
+    "url": "/alvaro-barrera/static/js/main.8de8b80c.chunk.js"
+  },
+  {
+    "revision": "9ca697ceb9c30251f5e8",
+    "url": "/alvaro-barrera/static/js/runtime~main.9ca697ce.js"
+  },
+  {
+    "revision": "598e66ef51ab57ec5497",
+    "url": "/alvaro-barrera/static/css/2.b83a50ef.chunk.css"
+  },
+  {
+    "revision": "598e66ef51ab57ec5497",
+    "url": "/alvaro-barrera/static/js/2.598e66ef.chunk.js"
+  },
+  {
+    "revision": "f6e54d53710dcf3b62e35925800f0d9c",
+    "url": "/alvaro-barrera/static/media/desarrollo-web.f6e54d53.png"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/alvaro-barrera/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "277ddd80c5dd140f56a6a7d0a62763f0",
+    "url": "/alvaro-barrera/static/media/profile.277ddd80.jpg"
+  },
+  {
+    "revision": "584d9f322f08684db8ff2217731e474f",
+    "url": "/alvaro-barrera/static/media/bootstrap.584d9f32.png"
+  },
+  {
+    "revision": "15a4866e67b5a9aac43ae3af08de6721",
+    "url": "/alvaro-barrera/static/media/javascript.15a4866e.png"
+  },
+  {
+    "revision": "c3ba82a536fe4983dad4c06b578dcd5d",
+    "url": "/alvaro-barrera/static/media/react.c3ba82a5.png"
+  },
+  {
+    "revision": "8bfc5015da73628edae2996b01ed50c0",
+    "url": "/alvaro-barrera/static/media/laravel.8bfc5015.png"
+  },
+  {
+    "revision": "44403f1eece67c892a0c6a6791da5ad4",
+    "url": "/alvaro-barrera/static/media/php.44403f1e.svg"
+  },
+  {
+    "revision": "ea92d6580dda0fbcf61a8ccb16e0d02a",
+    "url": "/alvaro-barrera/static/media/postgresql.ea92d658.png"
+  },
+  {
+    "revision": "07c28adf28e0592b46094aef81fcc655",
+    "url": "/alvaro-barrera/static/media/mysql.07c28adf.png"
+  },
+  {
+    "revision": "3e54ed15b9cd877c5223f5ecf64579df",
+    "url": "/alvaro-barrera/static/media/github.3e54ed15.png"
+  },
+  {
+    "revision": "63bc9e1a49c0c5e39ce3e01422c4cb20",
+    "url": "/alvaro-barrera/static/media/postman.63bc9e1a.svg"
+  },
+  {
+    "revision": "0d0645ecdac621aa6fd903a4e664d4cb",
+    "url": "/alvaro-barrera/static/media/phpstorm.0d0645ec.png"
+  },
+  {
+    "revision": "28d2fd31a112f9bc1faad68cf7ca857d",
+    "url": "/alvaro-barrera/static/media/vsc.28d2fd31.svg"
+  },
+  {
+    "revision": "26cd8d82884328bfa0733d841bf8b464",
+    "url": "/alvaro-barrera/static/media/solticrm.26cd8d82.PNG"
+  },
+  {
+    "revision": "7da95b7aaa2d54a71c1dc72d1b128dd5",
+    "url": "/alvaro-barrera/static/media/sistemalotes.7da95b7a.PNG"
+  },
+  {
+    "revision": "d514dc3ecd0cceb07a96d083d81bf65a",
+    "url": "/alvaro-barrera/static/media/classroom.d514dc3e.PNG"
+  },
+  {
+    "revision": "458a1530ec42765d1cc442b7a421d890",
+    "url": "/alvaro-barrera/static/media/web_bes.458a1530.PNG"
+  },
+  {
+    "revision": "dd37b6347abe07e592b6bee7385dc47a",
+    "url": "/alvaro-barrera/static/media/bes_efactura.dd37b634.PNG"
+  },
+  {
+    "revision": "5b68c4dc85a9d0f7c57e3475b6f7c98f",
+    "url": "/alvaro-barrera/static/media/idea.5b68c4dc.svg"
+  },
+  {
+    "revision": "fc5cb08f104f0ab077d86b796ba54340",
+    "url": "/alvaro-barrera/index.html"
+  }
+];
